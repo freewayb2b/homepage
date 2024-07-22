4ac7c4f5e5679f4868b38d287d8e90ad430e09dc
freewayb2b <b2bfreewayshoes@gmail.com>
2024-07-22 09:20:42 -0300

    a

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import pandas as pd
 link = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQAP3t48A4W5Afz-GJ7Jk7-uu7k7zVwIBB5CvuT_OIgooLFg9I0S9nmWYCkq3SwBQ/pub?gid=436659431&single=true&output=csv'
 
 
-df = pd.read_excel(link)
+df = pd.read_csv(link)
 
 st.table(df)
 
