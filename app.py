a4bf72a468ebea646a62eea1a21519b853d5148f
freewayb2b <b2bfreewayshoes@gmail.com>
2024-07-22 10:03:33 -0300

    a

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ import pandas as pd
 #--------------------------------------------------------------------
 #page-config
 
-st.set_page_config(title = "App teste")
+st.set_pageconfig(title = "App teste")
 
 
