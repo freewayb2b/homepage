32f8b5c1d8784bebc66231a0a9d4883baef78958
freewayb2b <b2bfreewayshoes@gmail.com>
2024-07-22 10:03:10 -0300

    a

--- app.py
+++ app.py
@@ -2,12 +2,9 @@ import streamlit as st
 import requests as rq
 import pandas as pd
 
+#--------------------------------------------------------------------
+#page-config
 
+st.set_page_config(title = "App teste")
 
-link = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQAP3t48A4W5Afz-GJ7Jk7-uu7k7zVwIBB5CvuT_OIgooLFg9I0S9nmWYCkq3SwBQ/pub?gid=436659431&single=true&output=csv'
-
-
-df = pd.read_csv(link)
-
-st.table(df)
 
