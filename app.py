ac20785d48ef7c371865e45474d1513d71522720
freewayb2b <b2bfreewayshoes@gmail.com>
2024-07-22 07:58:20 -0300

    a

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ import pandas as pd
 # link = "https://1drv.ms/x/s!Ap1U9e4-CgPGhcZ2MjrIMmz9riJXZQ?e=ZUqqMA=view"
 link = 'https://onedrive.live.com/read.aspx?resid=C6030A3EEEF5549D!90998&cid=c6030a3eeef5549d&CT=1721644678966&OR=ItemsView'
 
-df = rq.get(url=link,data='id="Sheet0_0_0_1"')
+# df = rq.get(url=link,data='id="Sheet0_0_0_1"')
 
-# df = pd.DataFrame(df)
+df = pd.DataFrame(df)
 
 st.table(df)
 
