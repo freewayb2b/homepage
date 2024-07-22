fadf1088e666da2e79fa22bc7df9cc3a248e670a
freewayb2b <b2bfreewayshoes@gmail.com>
2024-07-22 08:02:15 -0300

    a

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import pandas as pd
 
 # link = "https://1drv.ms/x/s!Ap1U9e4-CgPGhcZ2MjrIMmz9riJXZQ?e=ZUqqMA=view"
 link = 'https://1drv.ms/x/s!Ap1U9e4-CgPGhcZ2MjrIMmz9riJXZQ'
+link2 = 'https://1drv.ms/x/s!Ap1U9e4-CgPGhcZ2MjrIMmz9riJXZQ?download=1'
 
 # df = rq.get(url=link,data='id="Sheet0_0_0_1"')
 
