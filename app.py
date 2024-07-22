8e2ba9bce966d82b35e7657274a4e506ecd98206
freewayb2b <b2bfreewayshoes@gmail.com>
2024-07-22 08:04:09 -0300

    a

--- app.py
+++ app.py
@@ -8,9 +8,9 @@ import pandas as pd
 link = 'https://1drv.ms/x/s!Ap1U9e4-CgPGhcZ2MjrIMmz9riJXZQ'
 link2 = 'https://1drv.ms/x/s!Ap1U9e4-CgPGhcZ2MjrIMmz9riJXZQ?download=1'
 
-# df = rq.get(url=link,data='id="Sheet0_0_0_1"')
+df = rq.get(url=link2)
 
-df = pd.read_excel(link2)
+# df = pd.read_excel(link2)
 
 st.table(df)
 
