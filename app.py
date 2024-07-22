4b9d2e46dd54345be9a3701eda4d76dcb9bbe8a1
freewayb2b <b2bfreewayshoes@gmail.com>
2024-07-22 11:26:34 -0300

    a

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import requests as rq
 import pandas as pd
+from bs4 import BeautifulSoup
 
 #--------------------------------------------------------------------
 #page-config
@@ -9,6 +10,8 @@ st.set_page_config(layout="wide",page_title="app teste")
 
 link = "https://freewayb2b.shoppub.net/"
 
-tb = rq.get(url=link)
+tb = BeautifulSoup(link.text, 'html.parser')
+
+
 
 st.table(tb)
