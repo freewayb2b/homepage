c94b85620579b74a9e71aa589838a56e3ddd725e
freewayb2b <b2bfreewayshoes@gmail.com>
2024-07-22 07:55:29 -0300

    a

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import pandas as pd
 link = "https://1drv.ms/x/s!Ap1U9e4-CgPGhcZ2MjrIMmz9riJXZQ?e=ZUqqMA=view"
 # link = 'https://onedrive.live.com/edit.aspx?resid=C6030A3EEEF5549D!90998&cid=c6030a3eeef5549d&CT=1721644678966&OR=ItemsView'
 
-df = rq.get(url=link)
+df = pd.read_html(link)
 
 # df = pd.DataFrame(df)
 
