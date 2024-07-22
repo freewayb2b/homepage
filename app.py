09a6cd96a3383b6cdf5876ddd8a566405ecedb4d
freewayb2b <b2bfreewayshoes@gmail.com>
2024-07-22 07:59:02 -0300

    a

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 
 # link = "https://1drv.ms/x/s!Ap1U9e4-CgPGhcZ2MjrIMmz9riJXZQ?e=ZUqqMA=view"
-link = 'https://onedrive.live.com/read.aspx?resid=C6030A3EEEF5549D!90998&cid=c6030a3eeef5549d&CT=1721644678966&OR=ItemsView'
+link = 'https://onedrive.live.com/download.aspx?resid=C6030A3EEEF5549D!90998&cid=c6030a3eeef5549d&CT=1721644678966&OR=ItemsView'
 
 # df = rq.get(url=link,data='id="Sheet0_0_0_1"')
 
