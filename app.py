d97d113b6215905bedbc048fa2ec23cbf20f602d
freewayb2b <b2bfreewayshoes@gmail.com>
2024-07-22 08:05:52 -0300

    a

--- app.py
+++ app.py
@@ -5,11 +5,11 @@ import pandas as pd
 
 
 link = 'https://1drv.ms/x/s!Ap1U9e4-CgPGhcZ2MjrIMmz9riJXZQ'
-link2 = 'https://1drv.ms/x/s!Ap1U9e4-CgPGhcZ2MjrIMmz9riJXZQ?view=1'
+link2 = 'https://1drv.ms/x/s!Ap1U9e4-CgPGhcZ2MjrIMmz9riJXZQ?csv=1'
 
-df = rq.get(url=link2)
+# df = rq.get(url=link2)
 
-# df = pd.read_excel(link2)
+df = pd.read_csv(link2)
 
 st.table(df)
 
