1879167df497dc2d655f0af355c71693c8a1c873
freewayb2b <b2bfreewayshoes@gmail.com>
2024-07-23 10:45:11 -0300

    a

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-const a = "oi, eu sou o primeiro teste";
+const data = null;
 
-console.log(a);
+const xhr = new XMLHttpRequest();
+xhr.withCredentials = true;
+
+xhr.addEventListener('readystatechange', function () {
+	if (this.readyState === this.DONE) {
+		console.log(this.responseText);
+	}
+});
+
+xhr.open('GET', 'https://euribor.p.rapidapi.com/');
+xhr.setRequestHeader('x-rapidapi-key', 'bcd7f52af1msh6fbe910d79df94bp1e7ee4jsne94c78fcd98a');
+xhr.setRequestHeader('x-rapidapi-host', 'euribor.p.rapidapi.com');
+
+xhr.send(data);
